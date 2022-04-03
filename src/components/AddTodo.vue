3038f1d19c07d22761de8649f1a4f09bd1e917cb
ghulammustafa31299 <ghulam_mustafa@nextpak.org>
2022-04-03 23:47:17 +0500

    Initial commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,24 @@
+<template>
+  <input type="text" class="form-control add-todo" placeholder="Add todo" v-model="title" @keyup.enter="saveTask(title)" />
+</template>
+
+<script>
+import EventBus from '../EventBus';
+export default {
+	name: 'AddTodo',
+    data(){
+        return { 
+            title:''
+        }
+    },
+    methods:{
+        saveTask(title){
+            EventBus.$emit('add-todo',{title,id:Date.now().toString(),completed:false})
+            this.title=""
+        }
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/TodoListView.vue
@@ -0,0 +1,33 @@
+<template>
+	<ul id="sortable" class="list-unstyled">
+		<li class="ui-state-default" v-for="todo in todos" :key="todo.id">
+			<div class="checkbox">
+				<label>
+					<input type="checkbox" @click="finishedTodo(todo.id)"/>{{todo.title}}</label
+				>
+			</div>
+		</li>
+		
+	</ul>
+</template>
+
+<script>
+import EventBus from '../EventBus';
+export default {
+	name: 'TodoListView',
+	props: {
+		todos: {
+			type: Array,
+			required: true,
+		},
+	},
+	methods: {
+		finishedTodo(id) {
+			EventBus.$emit('completed-todo', { id: id });
+		},
+	},
+};
+</script>
+
+<style>
+</style>
